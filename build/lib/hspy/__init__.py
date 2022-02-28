412c38364f6443dec658ed8bd326982631d029cc
hyunsuk90 <hyunsuk.lee90@gmail.com>
2022-03-01 00:40:36 +0100

    no message

---
+++ build/lib/hspy/__init__.py
@@ -0,0 +1,9 @@
+# from .data import (DataReader)
+from .ttt import email_alert
+# from .data import (StockListing)
+# from .data import (EtfListing)
+# from . import (chart)
+
+__version__ = '0.0.1'
+
+__all__ = ['__version__', 'email_alert']
