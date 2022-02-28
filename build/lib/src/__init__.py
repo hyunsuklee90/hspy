43d504d8932f39cab470c2f2ad318f7c1f6058e9
hyunsuk90 <hyunsuk.lee90@gmail.com>
2022-03-01 00:46:58 +0100

    no message

--- build/lib/src/__init__.py
+++
@@ -1,9 +0,0 @@
-# from .data import (DataReader)
-from .ttt import email_alert
-# from .data import (StockListing)
-# from .data import (EtfListing)
-# from . import (chart)
-
-__version__ = '0.0.1'
-
-__all__ = ['__version__', 'email_alert']
