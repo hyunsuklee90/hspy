421a644fb0df91bcb08f9c0fa9e9d3e9d3e7fafe
hyunsuk90 <hyunsuk.lee90@gmail.com>
2022-03-01 00:28:14 +0100

    no message

--- build/lib/hspy/setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup, find_packages
-setup(name='hspy',
-      version='0.0.1',
-      package_dir = {'':'.'}, 
-      license = 'MIT',
-      author='hslee',
-      packages = find_packages(),
-      # install_requires = ['BeautifulSoup4', 'lxml']
-      )
