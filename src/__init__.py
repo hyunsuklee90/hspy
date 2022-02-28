9d94ea324f6a17787bc45a6778c177143634a181
hyunsuk90 <hyunsuk.lee90@gmail.com>
2022-03-01 00:31:26 +0100

    no message

--- src/__init__.py
+++ src/__init__.py
@@ -1,8 +1,9 @@
 # from .data import (DataReader)
+rom .ttt import email_alert
 # from .data import (StockListing)
 # from .data import (EtfListing)
 # from . import (chart)
 
 __version__ = '0.0.1'
 
-# __all__ = ['__version__', 'DataReader', 'StockListing', 'EtfListing', 'chart']
+__all__ = ['__version__', 'email_alert']
